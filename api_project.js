7710ecd033ed4d69beb91b06ec9a5171ab5f6808
Felipe Figueroa <amenadiel@gmail.com>
2014-10-13 09:46:58 -0300

    new page

---
+++ api_project.js
@@ -0,0 +1,9 @@
+define({
+  "name": "loopback-example-app",
+  "version": "3.0.0",
+  "description": "LoopBack Example Application",
+  "generator": {
+    "version": "0.7.2",
+    "time": "2014-10-13T12:45:34.266Z"
+  }
+});
